f85270a7a150851d6e05a6fcad51d9563fc17fd0
Kaktus2011 <ytkaktus2011@gmail.com>
2024-09-16 19:10:50 +0300

    update

--- Project2/BattleShipGame.h
+++ Project2/BattleShipGame.h
@@ -481,7 +481,8 @@ namespace morskoiBoi
 #pragma endregion
 	private:
 
-		int Ship = 4;
+		int Ship1 = 4;
+		int Ship2 = 3;
 	bool isHorizontal = true; // Ориентация корабля: true - горизонтальная, false - вертикальная
 	int currentShipSize = 0;   // Размер текущего корабля (устанавливается в зависимости от button1 - button4)
 	int Ship4 = 0;
